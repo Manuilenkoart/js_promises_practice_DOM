163c9432dc46a13d642c07ba121b647cb8c363a8
artur.manuilenko <artur.manuilenko@yalantis.net>
2025-01-22 21:12:39 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,41 +1,35 @@
 'use strict';
 
-const firstResolve = new Promise((resolve) => {
-  document.addEventListener('click', () => {
-    resolve('First promise was resolved');
-  });
-});
+const firstPromiseResolve = Promise.resolve('First promise was resolved');
+const secondPromise = Promise.resolve('Second promise was resolved');
+const thirdPromise = Promise.resolve('Third promise was resolved');
 
-const firstReject = new Promise((resolve, reject) => {
-  setTimeout(() => reject(new Error('First promise was rejected')), 3000);
-});
-
-Promise.race([firstResolve, firstReject])
-  .then((data) => promeseHandler(data, 'success'))
-  .catch((err) => promeseHandler(err, 'error'));
-
-const secondPromise = new Promise((resolve) => {
-  resolve('Second promise was resolved');
-});
-
-const thirdPromise = new Promise((resolve) => {
-  resolve('Third promise was resolved');
-});
-
-function promeseHandler(data, classStatus) {
+function showNotification(data, classStatus) {
   const div = document.createElement('div');
 
   div.setAttribute('data-qa', 'notification');
   div.classList.add(classStatus);
   div.textContent = data;
-
   document.body.appendChild(div);
 }
 
 let leftClickState = false;
 let rightClickState = false;
+let isRejectedFirstPromise = false;
 
-document.addEventListener('mouseup', (e) => {
+setTimeout(() => {
+  if (!leftClickState) {
+    const firstPromiseReject = Promise.reject(
+      new Error('First promise was rejected'),
+    );
+
+    firstPromiseReject
+      .catch((err) => showNotification(err.message, 'error'))
+      .finally(() => (isRejectedFirstPromise = true));
+  }
+}, 3000);
+
+document.addEventListener('mouseup', async (e) => {
   if (e.button === 0) {
     leftClickState = true;
   }
@@ -44,11 +38,23 @@ document.addEventListener('mouseup', (e) => {
     rightClickState = true;
   }
 
+  if (leftClickState && !isRejectedFirstPromise) {
+    const data = await firstPromiseResolve;
+
+    showNotification(data, 'success');
+  }
+
   if (leftClickState && rightClickState) {
-    thirdPromise.then((data) => promeseHandler(data, 'success'));
+    const data = await thirdPromise;
+
+    showNotification(data, 'success');
     leftClickState = false;
     rightClickState = false;
   }
 
-  secondPromise.then((data) => promeseHandler(data, 'success'));
+  if (leftClickState || rightClickState) {
+    const data = await secondPromise;
+
+    showNotification(data, 'success');
+  }
 });
